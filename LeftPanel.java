f6def2ea23a73087a844f90eebbd0d629bc34cb7
Bizary <42531883+Bizary@users.noreply.github.com>
2018-08-21 12:01:32 +0600

    Don't know what's happening

---
+++ LeftPanel.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.awt.*;
+
+public class LeftPanel extends JPanel implements ActionListener{
+
+    JLabel imgLabel1;
+    ImageIcon img;
+
+    public LeftPanel(){
+
+        super();
+        setBounds(0, 0 , 900, 800);
+        setLayout(null);
+
+        img = new ImageIcon("pic1.jpg");
+        imgLabel1 = new JLabel(img);
+        imgLabel1.setBounds(0, 0, 900, 800);
+        add(imgLabel1);
+
+      }
+
+        public void actionPerformed(ActionEvent ae){}
+}
